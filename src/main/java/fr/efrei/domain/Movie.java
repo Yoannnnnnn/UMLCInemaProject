722ca82b22a7dec43504fd2511bbb78df9c349ce
Yoannnnnnn <yoann.pho@efrei.net>
2023-11-19 18:35:11 +0100

    I updated Movie class

--- src/main/java/fr/efrei/domain/Movie.java
+++ src/main/java/fr/efrei/domain/Movie.java
@@ -6,9 +6,7 @@ public class Movie {
     private int duration;
     private int schedule;
 
-    private Movie (){
 
-    }
 
     public String getTitle() {
         return title;
@@ -26,6 +24,9 @@ public class Movie {
         return schedule;
     }
 
+    private Movie (){
+
+    }
     private Movie (MovieBuilder builder){
         this.age_restriction = builder.age_restriction;
         this.duration = builder.duration;
@@ -44,6 +45,26 @@ public class Movie {
             this.age_restriction = age_restriction;
             this.duration = duration;
             this.schedule = schedule;
+        }
+
+        public MovieBuilder setTitle(MovieBuilder builder) {
+            this.title = builder.title;
+            return this;
+        }
+
+        public MovieBuilder setAge_restriction(MovieBuilder builder) {
+            this.age_restriction = builder.age_restriction;
+            return this;
+        }
+
+        public MovieBuilder setDuration(MovieBuilder builder) {
+            this.duration = builder.duration;
+            return this;
+        }
+
+        public MovieBuilder setSchedule(MovieBuilder builder) {
+            this.schedule = builder.schedule;
+            return this;
         }
 
         public Movie build(){
