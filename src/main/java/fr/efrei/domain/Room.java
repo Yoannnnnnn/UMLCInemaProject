cc44899815c7641e5caaeda33074527e5207b8bd
yannickbarbe <yannick.barbe@efrei.net>
2023-11-19 20:15:37 +0200

    Room class created

--- src/main/java/fr/efrei/domain/Room.java
+++ src/main/java/fr/efrei/domain/Room.java
@@ -1,8 +1,34 @@
 package fr.efrei.domain;
 
 public class Room {
+ private int room_number;
+
+    private Room() {
+    }
+    private Room (Builder builder){
+        this.room_number= builder.room_number;
+    }
+
+    public int getRoom_number() {
+        return room_number;
+    }
+
     @Override
     public String toString() {
-        return super.toString();
+        return "Room{" +
+                "room_number=" + room_number +
+                '}';
+    }
+    public static class Builder{
+        private int room_number;
+
+        public Builder setRoom_number(int room_number) {
+            this.room_number = room_number;
+            return this;
+        }
+        public Room build(){
+            return new Room (this);
+        }
+
     }
 }
