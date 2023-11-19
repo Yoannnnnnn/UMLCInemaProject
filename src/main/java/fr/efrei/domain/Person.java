f8dc63fce7f1bf649db629fc027c52bd96b02791
louart13 <98977551+louart13@users.noreply.github.com>
2023-11-19 19:04:57 +0100

    Here's the completed Person Java Class

--- src/main/java/fr/efrei/domain/Person.java
+++ src/main/java/fr/efrei/domain/Person.java
@@ -1,5 +1,30 @@
 package fr.efrei.domain;
 
 public class Person {
+    private int age;
+    private Person(PersonBuilder builder){
+        this.age=builder.age;
+    }
+
+    private Person(){}
+    public int getAge() {
+        return age;
+    }
+
+    public static class PersonBuilder{
+        private int age;
+
+        public PersonBuilder(int age) {
+            this.age = age;
+        }
+
+        public PersonBuilder setAge(PersonBuilder builder) {
+            this.age = builder.age;
+            return this;
+        }
+        public Person build(){
+            return new Person(this);
+        }
+    }
 
 }
