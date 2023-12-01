6be9d79eedb5a8bb5414fe6cbf0022b6c166740c
Yoannnnnnn <yoann.pho@efrei.net>
2023-12-01 15:53:25 +0100

    MovieView update

--- src/main/java/fr/efrei/repository/IRepository.java
+++ src/main/java/fr/efrei/repository/IRepository.java
@@ -2,8 +2,7 @@ package fr.efrei.repository;
 
 public interface IRepository <T, string>{
     public T create(T t);
-    public T read(string movieCode);
+    public T read(string movie);
     public T update(T t);
-
-    public boolean delete(String seat_number);
+    public boolean delete(String title);
 }
