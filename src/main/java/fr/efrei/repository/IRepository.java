5376c57aa1f02a013a07a19045d70b205a3eca3c
yannickbarbe <yannick.barbe@efrei.net>
2023-12-01 17:25:41 +0200

    updates

--- src/main/java/fr/efrei/repository/IRepository.java
+++ src/main/java/fr/efrei/repository/IRepository.java
@@ -4,6 +4,6 @@ public interface IRepository <T, string>{
     public T create(T t);
     public T read(string movie);
     public T update(T t);
-    public boolean delete(String movie);
+    public boolean delete(String title);
 
 }
