9ae34f8684bbbf35b4ed191a92ae250e822920d7
Yoannnnnnn <yoann.pho@efrei.net>
2023-12-01 11:08:24 +0100

    MovieRepository update

--- src/main/java/fr/efrei/repository/IRepository.java
+++ src/main/java/fr/efrei/repository/IRepository.java
@@ -1,9 +1,9 @@
 package fr.efrei.repository;
 
-public interface IRepository <T, ID>{
+public interface IRepository <T, string>{
     public T create(T t);
-    public T read(ID id);
+    public T read(string movieCode);
     public T update(T t);
 
-    public boolean delete(ID id);
+    public boolean delete(String seat_number);
 }
---
+++ src/main/java/fr/efrei/repository/MovieRepository.java
@@ -0,0 +1,65 @@
+package fr.efrei.repository;
+
+import fr.efrei.domain.Movie;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MovieRepository implements IMovieRepository{
+    private static MovieRepository repository = null;
+    private List<Movie> movieDB = null;
+    private MovieRepository(){movieDB = new ArrayList<Movie>();}
+    public static MovieRepository getRepository(){
+        if (repository == null){
+            repository = new MovieRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public Movie create (Movie movie){
+        boolean success = movieDB.add(movie);
+        if(success){
+            return movie;
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public Movie read (String title){
+        for(Movie movie : movieDB){
+            if(movie.getTitle().equals(title)){
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Movie update (Movie newMovie){
+        Movie OldMovie = read(newMovie.getTitle());
+        if (OldMovie == null){
+            boolean successAdded = movieDB.add(newMovie);
+            if(successAdded){
+                return newMovie;
+            }
+            else{
+                return null;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete (String title){
+        Movie movieToDelete = read(title);
+        boolean success = movieDB.remove(movieToDelete);
+        return success;
+    }
+
+    @Override
+    public List <Movie> getAll(){
+        return movieDB;
+    }
+}
